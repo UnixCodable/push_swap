ae4a71a749aca34a884de5070b91722f23c95a88
Agathe Euvrard <aeuvrard@k1r2p2.42mulhouse.fr>
2025-12-20 15:23:48 +0100

    test: checking credentials

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: aeuvrard <aeuvrard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/20 02:25:40 by lbordana          #+#    #+#             */
-/*   Updated: 2025/12/20 15:22:30 by aeuvrard         ###   ########.fr       */
+/*   Updated: 2025/12/20 15:23:37 by aeuvrard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	error_handler(char *arg)
 {
 	int	i;
 
-	i = 1;
+	i = 0;
 	while (arg[i] != '\0')
 	{
 		if (arg[i] >= '0' && arg[i] <= '9')
