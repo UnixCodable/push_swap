d396efdd45d5cc5fe8fd0f27ee5c2b390d7ea433
UnixCodable <lucasbordanave@gmail.com>
2025-12-23 20:07:13 +0100

    fix: removed stack_b creation

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: lbordana <lbordana@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/20 02:25:40 by lbordana          #+#    #+#             */
-/*   Updated: 2025/12/23 14:36:10 by lbordana         ###   ########.fr       */
+/*   Updated: 2025/12/23 20:00:20 by lbordana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "libft/libft.h"
 #include <stdio.h>
 
-double	compute_disorder(t_num_list *stack_a)
+void	compute_disorder(t_num_list *stack_a, struct s_data *data)
 {
 	double	mistakes;
 	double	total_pairs;
@@ -28,34 +28,7 @@ double	compute_disorder(t_num_list *stack_a)
 			mistakes += 1;
 		stack_a = stack_a->next;
 	}
-	return (mistakes / total_pairs);
-}
-
-t_num_list	*create_stack_b(t_num_list *stack_a)
-{
-	t_num_list	*nbrs;
-	t_num_list	*prev;
-	t_num_list	*first;
-
-	if (!stack_a)
-	{
-		nbrs = NULL;
-		return (nbrs);
-	}
-	nbrs = ft_numlst_new(0);
-	first = nbrs;
-	prev = nbrs;
-	stack_a = stack_a->next;
-	while (stack_a)
-	{
-		nbrs->next = ft_numlst_new(0);
-		nbrs->is_empty = 1;
-		nbrs = nbrs->next;
-		nbrs->previous = prev;
-		prev = nbrs;
-		stack_a = stack_a->next;
-	}
-	return (first);
+	data->disorder = mistakes / total_pairs;
 }
 
 t_num_list	*create_stack_a(char **args, struct s_data *data)
@@ -81,21 +54,25 @@ t_num_list	*create_stack_a(char **args, struct s_data *data)
 	return (first);
 }
 
-// int	opt_checker(char *arg, t_data *data)
-// {
-// 	if (!ft_strncmp(*arg, "--simple", -1))
-// 		data->force_simple = 1;
-// 	else if (!ft_strncmp(*arg, "--medium", -1))
-// 		data->force_medium = 1;
-// 	else if (!ft_strncmp(*arg, "--complex", -1))
-// 		data->force_complex = 1;
-// 	else if (!ft_strncmp(*arg, "--adaptive", -1))
-// 		data->force_adaptive = 1;
-// 	else if (!ft_strncmp(*arg, "--bench", -1))
-// 		data->benchmark = 1;
-// }
+int	algorythm_checker(char *arg, struct s_data *data)
+{
+	if (!ft_strncmp(arg, "--simple", -1))
+		data->force_simple = 1;
+	else if (!ft_strncmp(arg, "--medium", -1))
+		data->force_medium = 1;
+	else if (!ft_strncmp(arg, "--complex", -1))
+		data->force_complex = 1;
+	else if (!ft_strncmp(arg, "--adaptive", -1))
+		data->force_adaptive = 1;
+	else
+	{
+		data->force_adaptive = 1;
+		return (0);
+	}
+	return (1);
+}
 
-int	error_handler(char **args)
+int	error_handler(char **args, struct s_data *data)
 {
 	static int	opt_off;
 	char		**voyager;
@@ -106,18 +83,18 @@ int	error_handler(char **args)
 		while (*voyager++)
 			if (*voyager && !ft_strncmp(*voyager, *args, -1))
 				return (0);
-		if (!ft_strncmp(*args, ft_itoa(ft_atoi(*args)), -1))
+		if (algorythm_checker(*args, data) && (!opt_off++))
+			args++;
+		else if (!ft_strncmp(*args, ft_itoa(ft_atoi(*args)), -1))
 		{
 			args++;
 			opt_off += 2;
 		}
-		else if ((!ft_strncmp(*args, "--simple", -1)
-				|| !ft_strncmp(*args, "--medium", -1)
-				|| !ft_strncmp(*args, "--complex", -1)
-				|| !ft_strncmp(*args, "--adaptive", -1)) && (!opt_off++))
-			args++;
 		else if (!ft_strncmp(*args, "--bench", -1) && opt_off < 2)
+		{
+			data->benchmark = 1;
 			args++;
+		}
 		else
 			return (0);
 	}
@@ -129,19 +106,48 @@ int	main(int ac, char **av)
 	t_num_list		*stack_a;
 	t_num_list		*stack_b;
 	struct s_data	data;
-	double			score;
 
 	(void) ac;
 	av++;
 	data = (struct s_data){0};
-	printf("%d", data.pa_count);
-	if (error_handler(av) == 0)
+	if (error_handler(av, &data) == 0)
 		return (write(2, "Error\n", 6), 0);
 	stack_a = create_stack_a(av, &data);
-	stack_b = create_stack_b(stack_a);
-	score = compute_disorder(stack_a);
-	printf("%d", data.pa_count);
-	(void) score;
+	stack_b = NULL;
+	compute_disorder(stack_a, &data);
+	printf("Bench = %d\n", data.benchmark);
+	printf("Simple = %d\n", data.force_simple);
+	printf("Medium = %d\n", data.force_medium);
+	printf("Complex = %d\n", data.force_complex);
+	printf("Adaptive = %d\n", data.force_adaptive);
+	printf("Disorder = %.2f\n", data.disorder);
 	(void) stack_b;
 	return (1);
 }
+
+// t_num_list	*create_stack_b(t_num_list *stack_a)
+// {
+// 	t_num_list	*nbrs;
+// 	t_num_list	*prev;
+// 	t_num_list	*first;
+
+// 	if (!stack_a)
+// 	{
+// 		nbrs = NULL;
+// 		return (nbrs);
+// 	}
+// 	nbrs = ft_numlst_new(0);
+// 	first = nbrs;
+// 	prev = nbrs;
+// 	stack_a = stack_a->next;
+// 	while (stack_a)
+// 	{
+// 		nbrs->next = ft_numlst_new(0);
+// 		nbrs->is_empty = 1;
+// 		nbrs = nbrs->next;
+// 		nbrs->previous = prev;
+// 		prev = nbrs;
+// 		stack_a = stack_a->next;
+// 	}
+// 	return (first);
+// }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: lbordana <lbordana@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/20 07:12:03 by lbordanave        #+#    #+#             */
-/*   Updated: 2025/12/23 14:38:38 by lbordana         ###   ########.fr       */
+/*   Updated: 2025/12/23 19:11:51 by lbordana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,22 +25,23 @@ typedef struct s_num_list
 
 struct s_data
 {
-	int	pa_count;
-	int	pb_count;
-	int	ra_count;
-	int	rb_count;
-	int	rr_count;
-	int	rra_count;
-	int	rrb_count;
-	int	rrr_count;
-	int	sa_count;
-	int	sb_count;
-	int	total_count;
-	int	force_simple;
-	int	force_medium;
-	int	force_complex;
-	int	force_adaptive;
-	int	benchmark;
+	int		pa_count;
+	int		pb_count;
+	int		ra_count;
+	int		rb_count;
+	int		rr_count;
+	int		rra_count;
+	int		rrb_count;
+	int		rrr_count;
+	int		sa_count;
+	int		sb_count;
+	int		total_count;
+	int		force_simple;
+	int		force_medium;
+	int		force_complex;
+	int		force_adaptive;
+	int		benchmark;
+	double	disorder;
 };
 
 t_num_list	*ft_numlst_new(int nbr);
