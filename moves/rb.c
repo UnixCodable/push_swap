ce2c3ee8743bd626b4b3f2f92ab847299fac0cc2
UnixCodable <lucasbordanave@gmail.com>
2025-12-24 02:18:52 +0100

    test: added temporary tester for movements

--- moves/rb.c
+++ moves/rb.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rb.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agathe <agathe@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lbordana <lbordana@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 15:13:28 by aeuvrard          #+#    #+#             */
-/*   Updated: 2025/12/24 00:59:30 by agathe           ###   ########.fr       */
+/*   Updated: 2025/12/24 01:55:35 by lbordana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	move_rb(t_num_list **stack_b, struct s_data *data)
 {
 	t_num_list	*voyager;
 
-    data->rb_count++;
+	data->rb_count++;
 	voyager = *stack_b;
 	while (voyager->next != NULL)
 		voyager = voyager->next;
--- testing_unit.c
+++ testing_unit.c
@@ -2,19 +2,89 @@
 
 void	testing(t_num_list **stack_a, t_num_list **stack_b, struct s_data *data)
 {
-	while ((*stack_a)->next || (*stack_b)->next)
+	(void) data;
+	/*PREMIERE IMPRESSION*/
+	while (((*stack_a) && (*stack_a)->next != NULL)|| ((*stack_b) && (*stack_b)->next != NULL))
 	{
 		if ((*stack_a))
+			printf("%d | ", (*stack_a)->nb);
+		else
+			printf("-");
+		if ((*stack_b))
+			printf(" %d\n", (*stack_b)->nb);
+		else
+			printf("-\n");
+		if ((*stack_a) && (*stack_a)->next)
+		{
 			(*stack_a) = (*stack_a)->next;
-		if ((*stack_b))
+			if ((*stack_a)->next == NULL)
+			{
+				printf("%d | ", (*stack_a)->nb);
+				if (!(*stack_b))
+					printf("-\n");
+			}
+		}
+		if ((*stack_b) && (*stack_b)->next)
+		{
 			(*stack_b) = (*stack_b)->next;
+			if ((*stack_b)->next == NULL)
+			{
+				if (!(*stack_a))
+					printf("-");
+				printf("%d\n", (*stack_a)->nb);
+			}
+		}
+	}
+	/*REMONTAGE*/
+	while (((*stack_a) && (*stack_a)->previous != NULL) || ((*stack_b) && (*stack_b)->previous != NULL))
+	{
+		if (*stack_a)
+			*stack_a = (*stack_a)->previous;
+		if (*stack_b)
+			*stack_b = (*stack_b)->previous;
+	}
+	printf("\n--------\n\n");
+	move_pb(stack_a, stack_b, data);
+	move_pb(stack_a, stack_b, data);
+	move_pb(stack_a, stack_b, data);
+	move_rrb(stack_b, data);
+	while (((*stack_a) && (*stack_a)->next != NULL)|| ((*stack_b) && (*stack_b)->next != NULL))
+	{
 		if ((*stack_a))
-			printf("%d", (*stack_a)->nb);
+			printf("%d | ", (*stack_a)->nb);
 		else
 			printf("-");
 		if ((*stack_b))
-			printf("%d", (*stack_b)->nb);
+			printf(" | %d\n", (*stack_b)->nb);
 		else
-			printf("-");
+			printf("-\n");
+		if ((*stack_a) && (*stack_a)->next)
+		{
+			(*stack_a) = (*stack_a)->next;
+			if ((*stack_a)->next == NULL)
+			{
+				printf("%d | ", (*stack_a)->nb);
+				if (!(*stack_b))
+					printf("-\n");
+			}
+		}
+		if ((*stack_b) && (*stack_b)->next)
+		{
+			(*stack_b) = (*stack_b)->next;
+			if ((*stack_b)->next == NULL)
+			{
+				if (!(*stack_a))
+					printf("-");
+				printf(" | %d\n", (*stack_b)->nb);
+			}
+		}
+	}
+	/*REMONTAGE*/
+	while (((*stack_a) && (*stack_a)->previous != NULL) || ((*stack_b) && (*stack_b)->previous != NULL))
+	{
+		if (*stack_a)
+			*stack_a = (*stack_a)->previous;
+		if (*stack_b)
+			*stack_b = (*stack_b)->previous;
 	}
 }
