dd0dbf258d1906a671f3b447e7082cd7e23c20a6
UnixCodable <lucasbordanave@gmail.com>
2025-12-26 03:39:38 +0100

    feat: created benchmark

--- benchmark.c
+++ benchmark.c
@@ -6,7 +6,7 @@
 /*   By: lbordanave <lbordanave@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/24 13:41:52 by lbordanave        #+#    #+#             */
-/*   Updated: 2025/12/25 08:36:09 by lbordanave       ###   ########.fr       */
+/*   Updated: 2025/12/26 03:38:08 by lbordanave       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,50 +14,51 @@
 
 void	create_header(void)
 {
-	ft_printf("\033[2J\033[H\033[1;32m");
-	ft_printf("╔═══════════════════════════════════════");
-	ft_printf("═══════════════════════════════════════╗\n");
-	ft_printf("║\t\t\t\t\t\t\t\t\t       ║\n");
-	ft_printf("║ ██████╗ ██╗   ██╗███████╗██╗  ██╗");
-	ft_printf("        ███████╗██╗    ██╗ █████╗ ██████╗   ║\n");
-	ft_printf("║ ██╔══██╗██║   ██║██╔════╝██║  ██║        ");
-	ft_printf("██╔════╝██║    ██║██╔══██╗██╔══██╗  ║\n");
-	ft_printf("║ ██████╔╝██║   ██║███████╗███████║        ");
-	ft_printf("███████╗██║ █╗ ██║███████║██████╔╝  ║\n");
-	ft_printf("║ ██╔═══╝ ██║   ██║╚════██║██╔══██║        ");
-	ft_printf("╚════██║██║███╗██║██╔══██║██╔═══╝   ║\n");
-	ft_printf("║ ██║     ╚██████╔╝███████║██║  ██║███████╗");
-	ft_printf("███████║╚███╔███╔╝██║  ██║██║       ║\n");
-	ft_printf("║ ╚═╝      ╚═════╝ ╚══════╝╚═╝  ╚═╝╚══════╝");
-	ft_printf("╚══════╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝       ║\n");
-	ft_printf("╠═══════════════════╦═══════════════════");
-	ft_printf("═══════════════════╦═══════════════════╣\n");
+	ft_printf("\e[2J\e[H\e[1;32m");
+	ft_printf("\n\n\
+  ██████╗ ██╗   ██╗███████╗██╗  ██╗        ███████╗██╗    ██╗ █████╗ ██████╗ \n\
+  ██╔══██╗██║   ██║██╔════╝██║  ██║        ██╔════╝██║    ██║██╔══██╗██╔══██╗\n\
+  ██████╔╝██║   ██║███████╗███████║        ███████╗██║ █╗ ██║███████║██████╔╝\n\
+  ██╔═══╝ ██║   ██║╚════██║██╔══██║        ╚════██║██║███╗██║██╔══██║██╔═══╝ \n\
+  ██║     ╚██████╔╝███████║██║  ██║███████╗███████║╚███╔███╔╝██║  ██║██║     \n\
+  ╚═╝      ╚═════╝ ╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝     \n\
+  ");
 	return ;
 }
 
-void	space_bound(int size, int dyn_space)
+void	pos_framing(void)
 {
-	while (size++ != dyn_space)
-		ft_printf(" ");
+	int	x;
+	int	y;
+
+	y = 1;
+	x = 0;
+	ft_printf("\e[%d;%dH╔", x, y++);
+	while (y != 80)
+		ft_printf("\e[%d;%dH═", x, y++);
+	ft_printf("\e[%d;%dH╗", x++, y);
+	while (++x != 29)
+		ft_printf("\e[%d;%dH║\e[%d;%dH║", x, y, x, 1);
+	y = 1;
+	ft_printf("\e[%d;%dH╠\e[%d;%dH╠\e[%d;%dH╚", 9, y, 26, y, 29, y);
+	while (++y != 80)
+		ft_printf("\e[%d;%dH═\e[%d;%dH═\e[%d;%dH═", 9, y, 26, y, 29, y);
+	ft_printf("\e[%d;%dH╣\e[%d;%dH╣\e[%d;%dH╝", 9, y, 26, y, 29, y);
+	y = 1;
+	x = 9;
+	while (++x != 26)
+		ft_printf("\e[%d;%dH║\e[%d;%dH║\e[%d;%dH\n", x, 25, x, 56, 29 ,80);
+	return ;
 }
 
 void	compute_benchmark(struct s_data *data)
 {
-	int	disorder;
+	//int	disorder;
 
-	disorder = (int)(data->disorder * 10000);
+	//disorder = (int)(data->disorder * 10000);
 	if (data->benchmark == 0)
 		return ;
 	create_header();
-	ft_printf("║ Disorder : %d.%d%%", disorder / 100, disorder % 100);
-	space_bound(ft_numlen(disorder % 100) + ft_numlen(disorder / 100), 5);
-	ft_printf("║\t\t  BENCHMARKING\t\t   ║       MOVES       ║\n");
-	ft_printf("╠═══════════════════╣\t\t\t\t\t   ╠═══════════════════╣");
-	ft_printf("║ Total operations  ║\t\t\t\t\t   ║ SA : %d\n", data->sa_count);
-	ft_printf("║        %d  ", data->total_count);
-	space_bound(ft_numlen(data->total_count), 9);
-	printf("║\t\t\t\t\t   ║ RA : %d", data->ra_count);
-	space_bound(ft_numlen(data->ra_count), 15);
-	printf("║");
+	pos_framing();
 	return ;
 }
