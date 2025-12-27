7c5a260c30d8a3a1bfa6fe7d33e7f0a5591d8fc7
Lucas Bordanave <lbordana@k1r4p1.42mulhouse.fr>
2025-12-27 12:10:50 +0100

    test: added move_ra

--- algorithm/complex.c
+++ algorithm/complex.c
@@ -6,7 +6,7 @@
 /*   By: lbordana <lbordana@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/24 04:18:01 by lbordanave        #+#    #+#             */
-/*   Updated: 2025/12/27 02:25:10 by lbordana         ###   ########.fr       */
+/*   Updated: 2025/12/27 12:08:00 by lbordana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void	complex_alg(t_nlist **st_a, t_nlist **st_b, struct s_data *data)
 {
-	t_nlist
 	(void)st_a;
 	(void)st_b;
 	(void)data;
