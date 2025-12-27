5317f1fe59da42535aa4c5b87deb69bc98bfdf6d
Lucas Bordanave <lbordana@k1r4p1.42mulhouse.fr>
2025-12-27 20:17:10 +0100

    added algoritm

--- algorithm/complex.c
+++ algorithm/complex.c
@@ -6,7 +6,7 @@
 /*   By: lbordana <lbordana@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/24 04:18:01 by lbordanave        #+#    #+#             */
-/*   Updated: 2025/12/27 12:08:00 by lbordana         ###   ########.fr       */
+/*   Updated: 2025/12/27 19:13:25 by lbordana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	complex_alg(t_nlist **st_a, t_nlist **st_b, struct s_data *data)
 {
-	(void)st_a;
-	(void)st_b;
-	(void)data;
+	t_nlist	*to_pivot;
+	
+	
 	return ;
 }
