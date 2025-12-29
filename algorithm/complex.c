c8aa1c80347d9fbe55e5026caccb6671091d8e45
UnixCodable <lucasbordanave@gmail.com>
2025-12-29 09:18:44 +0100

    finished bench

--- algorithm/complex.c
+++ algorithm/complex.c
@@ -6,7 +6,7 @@
 /*   By: lbordana <lbordana@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/24 04:18:01 by lbordanave        #+#    #+#             */
-/*   Updated: 2025/12/29 02:31:37 by lbordana         ###   ########.fr       */
+/*   Updated: 2025/12/29 09:17:51 by lbordana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,17 +35,32 @@ double	chunk_disorder(t_nlist *st_a, struct s_data *data, int chunk)
 void	complex_alg(t_nlist **st_a, t_nlist **st_b, struct s_data *data)
 {
 	static int	pivot;
+	int			chunk;
+	int			front;
+	t_nlist		*voyager;
+	t_nlist		*checker;
 
-	while ((*st_a))
+	voyager = *st_a;
+	checker = *st_a;
+	front = 0;
+	while (voyager->next != NULL)
 	{
-		rra(st_a, data, 1);
-		pivot = (*st_a)->nb;
-		while ((*st_a)->stop != 1)
+		while (checker != NULL)
 		{
-			(*st_a)->stop = 1;
-			if ((*st_a)->nb >= pivot)
-				pb(st_a, st_b, data);
-			rra(st_a, data, 1);
+			if (!front)
+				if (checker->nb > voyager->nb)
+					break ;
+			else if (front)
+				if (checker->nb < voyager->nb)
+					break ;
+			else if (checker->nb == voyager->nb)
+				front = 1;
+			checker = checker->next;
 		}
+		if (checker == NULL)
+			break ;
+		voyager = voyager->next;
+		checker = *st_a;
+		front = 0;
 	}
 }
