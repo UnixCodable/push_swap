3691d919d7cc3514f203ccdcfacce8a3859a47f6
Agathe Euvrard <aeuvrard@k1r2p3.42mulhouse.fr>
2026-01-10 17:37:30 +0100

    feat : algorithm medium very ok without improvements

--- algorithm/medium.c
+++ algorithm/medium.c
@@ -6,7 +6,7 @@
 /*   By: aeuvrard <aeuvrard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/24 04:17:40 by lbordanave        #+#    #+#             */
-/*   Updated: 2026/01/10 17:07:14 by aeuvrard         ###   ########.fr       */
+/*   Updated: 2026/01/10 17:25:09 by aeuvrard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -159,20 +159,14 @@ void	medium_alg(t_nlist **st_a, t_nlist **st_b, struct s_data *data, struct s_me
 		}
 	}
 	if (check_other_chunk(st_a) == 0)
-	{
 		while ((*st_a)->chunk != -1)
-		{
-			ra(st_a, data, 1);
-			ft_printf("chunk = %d\n", (*st_a)->chunk);
-		}
-	}
+			rra(st_a, data, 1);
 	while ((*st_b)->next && compute_disorder((*st_b), data) != 1)
 		simple_alg_chunk(st_a, st_b, data);
 	while ((*st_b))
 	{
 		pa(st_a, st_b, data);
 		(*st_a)->chunk = -1;
-		ft_printf("chunk = %d\n", (*st_a)->chunk);
 	}
 	if (compute_disorder((*st_a), data) != 0)
 		medium_alg(st_a, st_b, data, medium);
